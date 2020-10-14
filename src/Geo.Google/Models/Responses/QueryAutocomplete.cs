dfab14aab5d466b6e07bc9892fe5884850720979
JustinCanton <67930245+JustinCanton@users.noreply.github.com>
2020-10-13 22:55:42 -0400

    Autocomplete Results
    Adding models for the autocomplete results

---
+++ src/Geo.Google/Models/Responses/QueryAutocomplete.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="QueryAutocomplete.cs" company="Geo.NET">
+// Copyright (c) Geo.NET. All rights reserved.
+// </copyright>
+
+namespace Geo.Google.Models.Responses
+{
+    using System.Collections.Generic;
+    using Geo.Google.Converters;
+    using Geo.Google.Enums;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// A query autocomplete result returned by Google.
+    /// </summary>
+    public class QueryAutocomplete
+    {
+        /// <summary>
+        /// Gets or sets a unique identifier that can be used with other Google APIs.
+        /// </summary>
+        [JsonProperty("place_id")]
+        public string PlaceId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the human-readable name for the returned result.
+        /// For establishment results, this is usually the business name.
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets an array of terms identifying each section of the returned description.
+        /// </summary>
+        [JsonProperty("terms")]
+        public List<Term> Terms { get; } = new List<Term>();
+
+        /// <summary>
+        /// Gets an array with offset value and length.
+        /// These describe the location of the entered term in the prediction result text, so that the term can be highlighted if desired.
+        /// </summary>
+        [JsonProperty("matched_substrings")]
+        public List<MatchedSubstring> MatchedSubstrings { get; } = new List<MatchedSubstring>();
+    }
+}
---
+++ src/Geo.Google/Models/Responses/Term.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="Term.cs" company="Geo.NET">
+// Copyright (c) Geo.NET. All rights reserved.
+// </copyright>
+
+namespace Geo.Google.Models.Responses
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Identifies a section of the returned description.
+    /// </summary>
+    public class Term
+    {
+        /// <summary>
+        /// Gets or sets the start position of this term in the description.
+        /// </summary>
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+
+        /// <summary>
+        /// Gets or sets the text of the term.
+        /// </summary>
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
