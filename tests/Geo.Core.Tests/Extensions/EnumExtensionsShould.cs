905c87f9fabc054710b978225a1c0aacff84cf7d
JustinCanton <67930245+JustinCanton@users.noreply.github.com>
2020-10-19 22:32:04 -0400

    Moving Enum Test Cases
    Moved the enum test cases to their new home in Core

--- tests/Geo.Core.Tests/Extensions/EnumExtensionsShould.cs
+++ tests/Geo.Core.Tests/Extensions/EnumExtensionsShould.cs
@@ -2,12 +2,12 @@
 // Copyright (c) Geo.NET. All rights reserved.
 // </copyright>
 
-namespace Geo.Google.Tests.Extensions
+namespace Geo.Core.Tests.Extensions
 {
     using System;
     using FluentAssertions;
     using Geo.Core.Extensions;
-    using Geo.Google.Tests.Enums;
+    using Geo.Core.Tests.Enums;
     using NUnit.Framework;
 
     /// <summary>
