1d81028577a4d2b134b39c256a6443b6ad843aa2
JustinCanton <67930245+JustinCanton@users.noreply.github.com>
2020-11-10 20:15:29 -0500

    Adding converter tests
    Adding unit tests for the converter functionality

---
+++ tests/Geo.Here.Tests/Models/AutosuggestObject.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="AutosuggestObject.cs" company="Geo.NET">
+// Copyright (c) Geo.NET. All rights reserved.
+// </copyright>
+
+namespace Geo.Here.Tests.Models
+{
+    using Geo.Here.Converters;
+    using Geo.Here.Models.Responses;
+    using Newtonsoft.Json;
+
+    public class AutosuggestObject
+    {
+        [JsonConverter(typeof(AutosuggestJsonConverter))]
+        public BaseLocation Autosuggest { get; set; }
+    }
+}
