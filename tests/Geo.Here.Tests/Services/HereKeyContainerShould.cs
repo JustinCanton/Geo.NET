87b366d79a7b6b7620f0a5f4740dc5750fbbd0ea
JustinCanton <67930245+JustinCanton@users.noreply.github.com>
2020-11-10 19:52:28 -0500

    Adding key container here tests
    Adding unit tests for the key container

---
+++ tests/Geo.Here.Tests/Services/HereKeyContainerShould.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="HereKeyContainerShould.cs" company="Geo.NET">
+// Copyright (c) Geo.NET. All rights reserved.
+// </copyright>
+
+namespace Geo.Here.Tests.Services
+{
+    using FluentAssertions;
+    using Geo.Here.Services;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Unit tests for the <see cref="HereKeyContainer"/> class.
+    /// </summary>
+    [TestFixture]
+    public class HereKeyContainerShould
+    {
+        /// <summary>
+        /// Tests the key is properly set.
+        /// </summary>
+        [Test]
+        public void SetKey()
+        {
+            var keyContainer = new HereKeyContainer("abc123");
+            keyContainer.GetKey().Should().Be("abc123");
+        }
+    }
+}
